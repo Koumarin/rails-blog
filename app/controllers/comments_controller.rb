33863b2c31172eaf6ddd39ea4a723fc69fc63c2b
Koumarin <alicefigueiredo837@gmail.com>
2023-06-16 17:14:14 -0300

    add user ref to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    @comment.user = current_user
+    @comment.save
     redirect_to article_path(@article)
   end
 
@@ -14,6 +18,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :status)
+    params.require(:comment).permit(:body, :status)
   end
 end
